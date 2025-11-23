ab4e4fe57bc25b56aa74c3d607351535683ae8e9
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-11-23 11:04:29 +0100

    style: style message board

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const newMessageRoute = require('./routes/messageRoute')
 
 app.set("view engine", "ejs")
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 app.use('/new', newMessageRoute);
