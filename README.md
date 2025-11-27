48889c624970e82d0e3ed6824a3596b09484678d
Hubert Fratczak <hubert.fratczak@onet.com.pl>
2025-11-27 16:56:04 +0100

    feat: handle form validation by express-validator

---
+++ README.md
@@ -0,0 +1,15 @@
+## Mini message board
+
+# Description
+A message window project that allows you to read and send messages in real time using the Express server and the Postgresql Database.
+
+# Technologies
+express.js
+express-validator
+ejs
+pg
+dotenv
+vercel
+
+# Live preview
+
--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const newMessageRoute = express.Router();
 const db = require('../db/queries')
+const { body, validationResult, matchedData } = require("express-validator")
+
+const validateUser = [
+    body("author").trim()
+        .isLength({min: 3, max: 15}).withMessage("Username must be between 3 and 15 characters"),
+    body("text").trim()
+        .isAlpha().withMessage("Message must contains only letters")
+        .isLength({max: 200}).withMessage("Message can't be longer than 200 characters"),
+]
+
 newMessageRoute.get("/", (req, res) => {
-    res.render("form")
+    res.render("form", {msg: undefined} )
 })
 
-newMessageRoute.post("/", async (req, res) => {
-    reqText = req.body.text;
-    reqAuthor = req.body.author;
-    await db.insertIntoMessages(reqAuthor, reqText)
+newMessageRoute.post("/", [validateUser,  async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        let errMsg;
+        errors.array().forEach(err => {
+            errMsg = err.msg;
+        })
+        return res.status(400).render("form", {msg: errMsg} )
+    }
+    const { author, text } = matchedData(req)
+    await db.insertIntoMessages(author, text)
     res.redirect('/')
-});
+} ]);
 
 newMessageRoute.get("/message/:id", async (req, res) => {
     const id = req.params.id;
